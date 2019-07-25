966f6101e86f44b78b9b98b002c23cdaf416cb21
naayoung <ask5046a@gmail.com>
2019-07-25 21:59:54 +0900

    syntax

---
+++ syntax/string.py
@@ -0,0 +1,18 @@
+print('Hello world')
+print("Hello world")
+print("Hell'o' world")
+
+# escape
+print("Hell'o' \"w\"orld")
+
+# newline
+print('H\ne\nl\nl\no')
+
+# docstring
+print('''
+H
+e
+l
+l
+o
+''')
