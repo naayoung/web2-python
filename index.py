768d441750473d363ec64513780c735c9981a640
naayoung <ask5046a@gmail.com>
2019-08-06 18:58:03 +0900

    index

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 #!C:\Users\nayoung\AppData\Local\Programs\Python\Python37-32\python.exe
+import cgi
 print("Content-Type: text/html")
 print()
+form = cgi.FieldStorage()
+pageID = form["id"].value
 print('''<!DOCTYPE html>
 <html>
   <head>
@@ -8,13 +11,13 @@ print('''<!DOCTYPE html>
     <meta charset="euc-kr" />
   </head>
   <body>
-    <h1><a href="index.html">WEB</a></h1>
+    <h1><a href="index.py">WEB</a></h1>
     <ol>
-      <li><a href="1.html">HTML</a></li>
-      <li><a href="2.html">CSS</a></li>
-      <li><a href="3.html">JavaScript</a></li>
+      <li><a href="index.py?id=HTML">HTML</a></li>
+      <li><a href="index.py?id=CSS">CSS</a></li>
+      <li><a href="index.py?id=JavaScript">JavaScript</a></li>
     </ol>
-    <h2>나옹월드</h2>
+    <h2>{title}</h2>
     <p>
       Welcome~ My name is NaYoung! This is
       <a href="https://www.naver.com/">naver</a>.
@@ -34,28 +37,6 @@ print('''<!DOCTYPE html>
     <p style="margin-top:40px;">
       <strong>Happy <u>NaYoung</u> world</strong>
     </p>
-    <p>
-        <div id="disqus_thread"></div>
-        <script>
-        
-        /**
-        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-        /*
-        var disqus_config = function () {
-        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-        };
-        */
-        (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://web1-tspvdzouqf.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-        })();
-        </script>
-        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    </p>
   </body>
 </html>
-''')
+'''.format(title=pageID))
