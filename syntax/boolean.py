408bcf0034986acd9326ada4d78ffe27e4eeae80
naayoung <ask5046a@gmail.com>
2019-08-06 19:36:01 +0900

    boolean

---
+++ syntax/boolean.py
@@ -0,0 +1,25 @@
+# Number
+import os.path
+print(1)  # Integer
+
+# String
+print('Hello World')
+
+# Boolean
+print(True)  # 참
+print(False)  # 거짓
+
+# Expression
+print(1+1)
+print('Hello ' + 'World')  # Hello World
+
+# Comparison operator
+print(1 == 1)  # True
+print(1 < 2)  # True
+print(2 < 1)  # False
+
+# Membership operator
+print('world' in 'Hello world')  # True
+
+
+print(os.path.exists('boolean2.py'))
