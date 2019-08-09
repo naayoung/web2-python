013d262e690e791758356504621e8b50d5a7bd06
naayoung <ask5046a@gmail.com>
2019-08-09 18:44:16 +0900

    conditional

--- syntax/conditional.py
+++ syntax/conditional.py
@@ -8,10 +8,17 @@ else:
     print('Who are you?')
 '''
 
+'''
 if user_id == 'egoing':
     if user_pwd == '111111':
         print('Hello master')
     else:
         print('Who are you?')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'egoing' and user_pwd == '111111':
+    print('Hello master')
 else:
     print('Who are you?')
